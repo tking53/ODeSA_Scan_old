5fdfd160d94934a838bb7839cfd4b1881b1cd62e
Karl Smith <ksmith@lanl.gov>
2021-05-11 14:12:52 -0600

    Removed j from global scope. Removed unused ifs.

--- src/Scan.cpp
+++ src/Scan.cpp
@@ -54,15 +54,15 @@ typedef struct
 } Can;
 
 int Scan (){
-	static std::array< Can, 13 > det;
+	static std::array< Can, 13 > dets;
 	bool beamON, trg;
 
 	const int numFiles = 13;
-	std::array< ifstream, numFiles > fp;
+	std::array< ifstream, numFiles > fps;
 
 	string fileheader;
 
-	int j,k, pposition,
+	int k, pposition,
     Tracelength,
     eventlength;
 
@@ -191,8 +191,8 @@ int Scan (){
 
   TTree *tt = new TTree("T", fileheader.c_str());
 
-	for (int i=0;i < det.size(); i++) {
-		tt->Branch(("d" + std::to_string(i)).c_str(), &det.at(i),"l:s:amp:cfd:psd:trg");
+	for (int i=0;i < dets.size(); i++) {
+		tt->Branch(("d" + std::to_string(i)).c_str(), &dets.at(i),"l:s:amp:cfd:psd:trg");
 	}
 	//tt->Branch("runtime",&runtime,"Runtime (ms)");     // Runtime in ms
 	//tt->Branch("X",&steerer_X,"X steerer");
@@ -217,9 +217,9 @@ int Scan (){
 	for (int i = 0; i < numFiles; i++) {
 		openfile = prefix + "_wave" + to_string(i) + ".dat";
 		cout << " Opening file: " << openfile;
-		fp[i].open(openfile, std::ifstream::in | std::ifstream::binary);
+		fps[i].open(openfile, std::ifstream::in | std::ifstream::binary);
 
-		if(fp[i].is_open()) {cout << " - Open!" << endl;}
+		if (fps[i].is_open()) {cout << " - Open!" << endl;}
 		else {{cout << " - Failed!" << endl;} }
 	}
 
@@ -233,119 +233,113 @@ int Scan (){
 
 	while (true) {
 		beamON = 0;
-		for (j = 0; j < numFiles; j++) {
-			if(j > -1) {
-				//if(!fp[j].is_open()){cout << "Could not open file!!" << endl; break;}
+		for (int detNum=0; detNum < numFiles; detNum++) {
+			// Stop after nth events...
+			//if (TEvt > 1000000) {break;}
 
-				// Stop after nth events...
-				//if (TEvt > 1000000) {break;}
+			trace_min = 0;
 
-				if(fp[j].is_open()) {
+			// Binary parsing
+			auto & fp = fps[detNum];
 
-					trace_min = 0;
+			if (!fp.read((char*)&buffer32, 4)) {break;}
+			Tracelength = (buffer32 - 16)/2;
+			fp.read((char*)&buffer32, 4);
+			fp.read((char*)&buffer32, 4);
+			fp.read((char*)&buffer32, 4);
 
-					// Binary parsing
-					if (!fp[j].read((char*)&buffer32, 4)) {break;}
-					Tracelength = (buffer32 - 16)/2;
-					fp[j].read((char*)&buffer32, 4);
-					fp[j].read((char*)&buffer32, 4);
-					fp[j].read((char*)&buffer32, 4);
+			// Trigger time in 2 ADC clock cycles ( 8 ns )
+			if (detNum == 0) trgtime = buffer32;
 
-					// Trigger time in 2 ADC clock cycles ( 8 ns )
-					if (j == 0) trgtime = buffer32;
+			// Reset variables
+			CFD = -1;
+			amplitude = -1;
+			paraL = 0;
+			paraS = 0;
+			trg = 0;
+			tac = 0;
+			pposition = -1;
+			//steerer_X = 0;
+			//steerer_Y = 1;
+			temp = 0;
 
-					// Reset variables
-					CFD = -1;
-					amplitude = -1;
-					paraL = 0;
-					paraS = 0;
-					trg = 0;
-					tac = 0;
-					pposition = -1;
-					//steerer_X = 0;
-					//steerer_Y = 1;
-					temp = 0;
+			// Get traces
+			for (int i = 0; i < Tracelength; i++) {
+				if (!fp.read((char*)&buffer16, 2)) {break;}
+				if (detNum < 12) {
+					pulse.push_back(16383 - (float) buffer16);
+				}
+				else {pulse.push_back(buffer16);}
 
-					// Get traces
-					for (int i = 0; i < Tracelength; i++) {
-						if (!fp[j].read((char*)&buffer16, 2)) {break;}
-						if (j < 12) {
-							pulse.push_back(16383 - (float) buffer16);
-						}
-						else {pulse.push_back(buffer16);}
+				if (pulse.back() > (16383 - threshold[detNum])) {trg = 1;}
 
-						if (pulse.back() > (16383 - threshold[j])) {trg = 1;}
+				// Added traces
+				if (detNum==0) {trace0->SetBinContent(i, pulse.back());}
+				else if (detNum==1) {trace1->SetBinContent(i, pulse.back());}
+			}
 
-						// Added traces
-						if (j==0) {trace0->SetBinContent(i, pulse.back());}
-						if (j==1) {trace1->SetBinContent(i, pulse.back());}
+			/** Liquid can processing **/
+			if(Tracelength > 1) {
+				// Process trace
+				// Get a Continuous Moving Average (CMA) of the pulse.
+				CMAtrace = Analysis->CMA_Filter(pulse, 10, pulse[0], 3.5 );
+				// Subtract the CMA from the pulse
+				std::transform(pulse.begin(), pulse.end(), CMAtrace.begin(), pulse.begin(), std::minus<int>());
+
+				// Find the maximum element in the subtracted pulse.
+				auto maxElement = std::max_element(pulse.begin(), pulse.end());
+				amplitude = *maxElement;
+				pposition = std::distance(pulse.begin(), maxElement);
+
+				// Fill the bins.
+				for (size_t i=0; i < pulse.size(); i++) {
+					auto & value = pulse[i];
+					if (detNum==0) {trace0->SetBinContent(i, value);}
+					else if (detNum==1) {trace1->SetBinContent(i, value);}
+					trace0C->SetBinContent(i, value);
+				}
+				for (size_t i=0; i < CMAtrace.size(); i++) {
+					traceCMA->SetBinContent(i, CMAtrace[i]);
+				}
+
+
+				// CFD timing
+				//f1->SetParameters(1.0, (double)pposition, 0.1);
+				//trace0->GetXaxis()->SetRangeUser(pposition - 5, pposition + 1);
+				//trace0->Fit("f1","RQ");
+				//float mu = (float)f1->GetParameter(1);
+				//float sigma = (float)f1->GetParameter(2);
+
+				//CFD = mu - sqrtf(1.38629*sigma*sigma);
+				//CFD = (float)pposition;
+
+				// PSD integration
+				float offset = 12.0; // original 12
+				if (pposition - 10 > 0 && pposition + 100 < Tracelength) {
+					for (int i = (pposition - 10); i < (pposition + 100); i++) {
+						paraL += pulse[i];
+						if (i > pposition + offset) { paraS += pulse[i];}
 					}
-
-					/** Liquid can processing **/
-					if(Tracelength > 1) {
-						// Process trace
-						if (j < numFiles) {
-							// Get a Continuous Moving Average (CMA) of the pulse.
-							CMAtrace = Analysis->CMA_Filter(pulse, 10, pulse[0], 3.5 );
-							// Subtract the CMA from the pulse
-							std::transform(pulse.begin(), pulse.end(), CMAtrace.begin(), pulse.begin(), std::minus<int>());
-
-							// Find the maximum element in the subtracted pulse.
-							auto maxElement = std::max_element(pulse.begin(), pulse.end());
-							amplitude = *maxElement;
-							pposition = std::distance(pulse.begin(), maxElement);
-
-							// Fill the bins.
-							for (size_t i=0; i < pulse.size(); i++) {
-								auto & value = pulse[i];
-								if (j==0) {trace0->SetBinContent(i, value);}
-								if (j==1) {trace1->SetBinContent(i, value);}
-								trace0C->SetBinContent(i, value);
-							}
-							for (size_t i=0; i < CMAtrace.size(); i++) {
-								traceCMA->SetBinContent(i, CMAtrace[i]);
-							}
+				}
+			}
 
 
-          // CFD timing
-          //f1->SetParameters(1.0, (double)pposition, 0.1);
-          //trace0->GetXaxis()->SetRangeUser(pposition - 5, pposition + 1);
-          //trace0->Fit("f1","RQ");
-          //float mu = (float)f1->GetParameter(1);
-          //float sigma = (float)f1->GetParameter(2);
+			auto & det = dets.at(detNum);
+			det.amp = amplitude;
+			det.l = paraL;
+			det.s = paraS;
+			det.cfd = CFD;
+			det.trg = trg;
+			det.psd = paraS / paraL;
+			if (det.trg) {det.trg = 1;}
+			else {det.trg = 0;}
 
-          //CFD = mu - sqrtf(1.38629*sigma*sigma);
-	        //CFD = (float)pposition;
-
-          // PSD integration
-          float offset = 12.0; // original 12
-          if (pposition - 10 > 0 && pposition + 100 < Tracelength) {
-            for (int i = (pposition - 10); i < (pposition + 100); i++) {
-              paraL += pulse[i];
-					    if (i > pposition + offset) { paraS += pulse[i];}
-				    }
-          }
-
-          }
-        }
-	    }
-
-
-		det.at(j).amp = amplitude;
-		det.at(j).l = paraL;
-		det.at(j).s = paraS;
-		det.at(j).cfd = CFD;
-		det.at(j).trg = trg;
-		det.at(j).psd = paraS / paraL;
-		if (det.at(j).trg) {det.at(j).trg = 1;}
-		else {det.at(j).trg = 0;}
-
-		switch(j) {
-			case 0 :
-				// Runtime clock
-				difftime = trgtime - prevtime;
-				if(difftime < 0) {
-					difftime = difftime + 2147483647;
+			switch(detNum) {
+				case 0 :
+					// Runtime clock
+					difftime = trgtime - prevtime;
+					if(difftime < 0) {
+						difftime = difftime + 2147483647;
 					runtime += ((8*difftime)/1.0E6);
 					prevtime = trgtime;
 				}
@@ -367,24 +361,23 @@ int Scan (){
 				break;
 		}
 
-			}
 		}
 		tt->Fill();
       TEvt++;
       if (TEvt%1000==0) {cout << "\rEvent counter: " << TEvt << flush;}
 
-}
+	}
 
-  for (int i = 0; i < numFiles; i++) {
-      if(fp[j].is_open()) fp[j].close();
-    }
-  ff->cd();
-  tt->Write();
-  ff->Close();
+	for (auto & fp : fps) {
+		if(fp.is_open()) fp.close();
+	}
+	ff->cd();
+	tt->Write();
+	ff->Close();
 
-  cout << "\nFinished! " << TEvt << " events" << endl;
+	cout << "\nFinished! " << TEvt << " events" << endl;
 
-  return 0;
+	return 0;
 }
 
 int main() {
